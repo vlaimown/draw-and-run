a10f74cc8a00faa66368d5557f084202b9c90d76
Vlad Stepanov <v1ad5tepanov@mail.ru>
2023-12-02 02:34:17 +1000

    добавлены префабы окна конца игры

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] private Player player;
     [SerializeField] private Transform finish;
+
+    // отрефакторить
+    [SerializeField] private TMP_Text victoryScoreText;
+    [SerializeField] private TMP_Text defeatScoreText;
 
     [SerializeField] private GameObject victoryWindow;
     [SerializeField] private GameObject defeatWindow;
@@ -17,10 +22,16 @@ public class GameController : MonoBehaviour
             player.Move(finish);
 
         if (player.HeroAlivesCount() == 0)
+        {
             defeatWindow.SetActive(true);
+            defeatScoreText.text = $"Score: {score.Value}";
+        }
 
         if (Vector3.Distance(player.transform.position, finish.position) <= 0.05f)
+        {
             victoryWindow.SetActive(true);
+            victoryScoreText.text = $"Score: {score.Value}";
+        }
     }
 
     public void ReloadScene()
