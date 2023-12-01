b4d87c4dca1684e3416551c784c733e5df4f2e3e
Vlad Stepanov <v1ad5tepanov@mail.ru>
2023-12-02 02:21:18 +1000

    добавлен подбор элементов

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    private Score score;
+    [SerializeField] private int value;
+
+    private void Start()
+    {
+        score = FindAnyObjectByType<Score>();
+    }
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.gameObject.CompareTag("Hero"))
+        if (other.gameObject.CompareTag("Hero"))
+        {
+            score.Value += value;
+            Destroy(gameObject);
+        }
     }
 }
